feec2e45bb82999f7cad37745243d2d9cc8dffe5
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-03-21 11:42:12 +0900

    編集ページに遷移したときに販売手数料、販売利益が表示される処理を追記

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,12 +1,16 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
+  TaxProfitCalc(priceInput);
   priceInput.addEventListener("input",function(){
-    const inputValue = priceInput.value;
-    const tax = 0.1
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(inputValue * tax);
-    const addProfit = document.getElementById("profit");
-    addProfit.innerHTML = inputValue - Math.floor(inputValue * tax);
+    TaxProfitCalc(priceInput);
   });
 });
 
+function TaxProfitCalc(priceInput) {
+  const inputValue = priceInput.value;
+  const tax = 0.1
+  const addTaxDom = document.getElementById("add-tax-price");
+  addTaxDom.innerHTML = Math.floor(inputValue * tax);
+  const addProfit = document.getElementById("profit");
+  addProfit.innerHTML = inputValue - Math.floor(inputValue * tax);
+}
