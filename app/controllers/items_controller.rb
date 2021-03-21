63f081ad9d142a6a7b19d7fbb2469c6b03b99d4b
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-03-21 16:19:47 +0900

    move_to_indexアクションの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, expect: [:edit, :update]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -22,9 +23,6 @@ class ItemsController < ApplicationController
   end
   
   def edit
-    unless current_user.id == @item.user.id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -42,5 +40,11 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user.id == @item.user.id
+      redirect_to root_path
+    end
   end
 end
