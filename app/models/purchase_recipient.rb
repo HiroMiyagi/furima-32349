3bcb0b7d0489e8df7222a49549c99fc39b189d9d
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-04-03 19:05:36 +0900

    PurchaseRecipientフォームオブジェクトの作成

---
+++ app/models/purchase_recipient.rb
@@ -0,0 +1,16 @@
+class PurchaseRecipient
+  include ActiveModel::Model
+  attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase
+
+  #バリデーション
+  with_options presence: true do
+    validates :user
+    validates :item
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "にはハイフンを入れて入力してください" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "都道府県を入力してください" }
+    validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
+    validates :address, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'には全角文字を使用してください' }
+    validates :phone_number, numericality: { only_integer: true }
+  end
+end
+
