fb6926bc140d5920f975c008baf8eb77ecba648c
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-04-11 18:32:52 +0900

    出品者が購入ページに遷移できない記述

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,13 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
+
   def index
     @purchase_recipient = PurchaseRecipient.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @purchase_recipient = PurchaseRecipient.new(purchase_params)
     if @purchase_recipient.valid?
       pay_item
@@ -30,5 +31,13 @@ class PurchasesController < ApplicationController
       card: purchase_params[:token],
       currency: 'jpy'
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user.id
   end
 end
