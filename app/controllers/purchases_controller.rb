25e41dd262b6e0911de1283121556563fcd7ccba
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-04-11 18:24:19 +0900

    ログアウト中のユーザーが購入画面に遷移できない機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
   def index
     @purchase_recipient = PurchaseRecipient.new
     @item = Item.find(params[:item_id])
