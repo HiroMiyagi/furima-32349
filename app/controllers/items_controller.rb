0059cad772ca1c38193d3d4122430070db897564
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-04-17 10:52:19 +0900

    コメント10修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    return redirect_to root_path if (current_user.id != @item.user.id) or (@item.purchase.present?)
+    return redirect_to root_path if (current_user.id != @item.user.id) || (@item.purchase.present?)
   end
 end
