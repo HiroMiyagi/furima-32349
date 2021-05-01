c4b1114059fa36dcf74e10ac1d3ed5e41c6a1a44
HiroMiyagi <isniilevsata_2012@yahoo.co.jp>
2021-05-01 18:38:29 +0900

    ビューの一覧表示機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
 
   private
   
-  def commemt_params
+  def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
